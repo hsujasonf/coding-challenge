9da32711620df0f6c85260c9a2bf4ccaf71af457
Jason Hsu <hsujasonf@gmail.com>
2024-03-13 12:06:51 -0700

    refactor async

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,28 +1,35 @@
 "use strict";
-const heapifyDown = require("../lib/heapify-down");
+const MinHeap = require("../lib/min-heap");
+
+async function populateMinHeap(minHeap, logSources) {
+  const promises = [];
+  logSources.forEach((logSource) => {
+    if (!logSource.drained) {
+      promises.push(pushLogEntryToHeap(minHeap, logSource));
+    }
+  });
+  return Promise.all(promises);
+}
+
+async function pushLogEntryToHeap(minHeap, logSource) {
+  const logEntry = await logSource.popAsync();
+  if (logEntry) {
+    minHeap.push(logEntry);
+  }
+}
 
 module.exports = async (logSources, printer) => {
-  // instead of building a new heap, heapify the logSources array
-  for (let i = Math.floor(logSources.length / 2) - 1; i >= 0; i--) {
-    heapifyDown(logSources, i);
-  }
+  let minHeap = new MinHeap();
+  // start with populating min heap by popping from every logSource
+  await populateMinHeap(minHeap, logSources);
 
-  while (logSources.length) {
-    // printing logSources[0].pop won't log the first 'last' log so I chose to print logSource[0].last and then call pop()
-    printer.print(logSources[0].last);
-    //  I know it defeats the purpose of the task but since we're logging
-    //  logSources[0].last, we could technically just call logSources[0].popAsync() without await
-    //  and the last value of logSources[0]
-    //  would change without a delay.
-    await logSources[0].popAsync();
-    if (logSources[0].drained) {
-      logSources.shift();
-      const last = logSources.pop();
-      if (logSources.length > 0) {
-        logSources.unshift(last);
-      }
+  while (minHeap.heap.length) {
+    printer.print(minHeap.pop());
+    // min length is logSource length squared since when it hits
+    // a limit, it will push logSources.length at a time
+    if (minHeap.heap.length < logSources.length * logSources.length) {
+      await populateMinHeap(minHeap, logSources);
     }
-    heapifyDown(logSources, 0);
   }
   printer.done();
   return console.log("Async sort complete.");
